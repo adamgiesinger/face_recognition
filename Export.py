06a3f7e0e7b7a4c5a5410edfe197077503a0ecfd
Claudio Bianucci <claudio.bianucci@namics.com>
2018-09-17 13:59:23 +0200

    Export stub.

---
+++ Export.py
@@ -0,0 +1,14 @@
+def export(columns, rows):
+    print("TODO: write to csv")
+
+
+persons = ['one', 'two', 'three', 'four']
+
+recognitions = {
+    'a.jpg': ['one', 'two'],
+    'b.jpg': [],
+    'c.jpg': ['three'],
+    'd.jpg': ['one', 'two', 'three']
+}
+
+export(persons, recognitions)
