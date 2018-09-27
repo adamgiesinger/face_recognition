e68f81db652cfc9743b4da2a61711a4036e420de
Adam Giesinger <adam.giesinger@namics.com>
2018-09-27 21:44:59 +0200

    Initial commit

--- live_face_recognition.py
+++ live_face_recognition.py
@@ -1,34 +1,26 @@
 import face_recognition
 import cv2
-import sys
 import os
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
 
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+video_capture = cv2.VideoCapture(0)
+pathToKnownPeopleImages = "./known/"
 
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(sys.argv[2])
-
-pathToKnownPeopleImages = sys.argv[1]
-
-# Create arrays of known face encodings and their names
 known_face_encodings = []
 known_face_names = []
 
 for filename in os.listdir(pathToKnownPeopleImages):
+    if filename == ".DS_Store":
+        continue
     image = face_recognition.load_image_file(pathToKnownPeopleImages + filename)
-    face_encoding = face_recognition.face_encodings(image)[0]
-    known_face_encodings.append(face_encoding)
-    file_name = os.path.splitext(filename)[0]
-    known_face_names.append(file_name)
+    try:
+        face_encoding = face_recognition.face_encodings(image)[0]
+        known_face_encodings.append(face_encoding)
+        file_name = os.path.splitext(filename)[0]
+        known_face_names.append(file_name)
+    except IndexError:
+        print("Face not found in " + filename)
 
-# Initialize some variables
 face_locations = []
 face_encodings = []
 face_names = []
@@ -41,7 +33,7 @@ while True:
     # Resize frame of video to 1/4 size for faster face recognition processing
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    # Convert the image from BGR color (used by OpenCV) to RGB color (used by face_recognition)
     rgb_small_frame = small_frame[:, :, ::-1]
 
     # Only process every other frame of video to save time
@@ -59,7 +51,7 @@ while True:
             # If a match was found in known_face_encodings, just use the first one.
             if True in matches:
                 first_match_index = matches.index(True)
-                name = known_face_names[first_match_index]
+                name = known_face_names[first_match_index][:-1]
 
             face_names.append(name)
 
@@ -68,7 +60,7 @@ while True:
 
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
         top *= 4
         right *= 4
         bottom *= 4
@@ -85,10 +77,10 @@ while True:
             frame[top:bottom, left:right] = face_image
         else:
             # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.rectangle(frame, (left, top), (right, bottom), (147, 107, 13), 2)
 
             # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (147, 107, 13), cv2.FILLED)
             font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
